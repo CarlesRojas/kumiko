c3d55a6b67f073f39639b16fe45021783147b59f
CarlesRojas <carlesrojas@outlook.com>
2025-07-05 16:13:23 +0200

    fix

--- src/routes/$language/_protected/create/index.tsx
+++ src/routes/$language/_protected/create/index.tsx
@@ -10,7 +10,7 @@ function LoggedInHome() {
     return (
         <main className="mx-auto flex max-w-5xl flex-col gap-16">
             <h1 className="bg-wood-400 from-wood-400 to-wood-500 bg-gradient-to-r bg-clip-text py-2 text-center text-3xl leading-tight font-bold text-pretty text-transparent md:text-5xl lg:text-6xl">
-                Logged In
+                {t.landing.h1}
             </h1>
         </main>
     );
