c03fd7bad1f7f8c432568dba0d8a68b9f909d543
CarlesRojas <carlesrojas@outlook.com>
2025-07-06 10:33:50 +0200

    zoom and pan done

--- src/routes/$language/_protected/create/index.tsx
+++ src/routes/$language/_protected/create/index.tsx
@@ -1,6 +1,8 @@
 import { cn } from "@/lib/cn";
 import { createFileRoute } from "@tanstack/react-router";
-import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
+import { useRef, type RefObject } from "react";
+import { TransformComponent, TransformWrapper, type ReactZoomPanPinchRef } from "react-zoom-pan-pinch";
+import { useDebounceCallback, useResizeObserver } from "usehooks-ts";
 
 export const Route = createFileRoute("/$language/_protected/create/")({ component: LoggedInHome });
 
@@ -8,35 +10,58 @@ const rows = 10;
 const cols = 10;
 
 function LoggedInHome() {
+    const wrapperRef = useRef<HTMLDivElement>(null);
+    const gridRef = useRef<HTMLDivElement>(null);
+    const transformRef = useRef<ReactZoomPanPinchRef | null>(null);
+
+    const updateScale = () => {
+        const grid = gridRef.current;
+        const transform = transformRef.current;
+        if (grid && transform) transform.zoomToElement(grid, undefined, 100);
+    };
+    const onResize = useDebounceCallback(updateScale, 200);
+    useResizeObserver({ ref: wrapperRef as RefObject<HTMLElement>, onResize });
+
     return (
-        <TransformWrapper centerOnInit minScale={0.1} maxScale={2}>
-            <TransformComponent wrapperClass="relative !h-full !w-full">
-                <div className="grid w-fit" style={{ gridTemplateColumns: `repeat(${cols}, minmax(0, 1fr))` }}>
-                    {[...Array(rows)].map((_, row) =>
-                        [...Array(cols)].map((_, col) => (
-                            <div
-                                key={`${row}-${col}`}
-                                className={cn(
-                                    "bg-wood-200 -mt-[50px] h-[100px] w-[86.6px] scale-[0.89] hover:opacity-80",
-                                    row === 0 && "mt-0",
-                                    (row + col) % 2 === 0
-                                        ? "triangle-left origin-[66.5%_50%]"
-                                        : "triangle-right origin-[33.4%_50%]",
+        <div ref={wrapperRef} className="relative h-full w-full">
+            <TransformWrapper
+                ref={transformRef}
+                centerOnInit
+                minScale={0.01}
+                maxScale={10}
+                doubleClick={{ disabled: true }}
+                zoomAnimation={{ animationTime: 100 }}
+                alignmentAnimation={{ animationTime: 100 }}
+            >
+                <TransformComponent wrapperClass="relative !h-full !w-full ">
+                    <div
+                        ref={gridRef}
+                        className="grid w-fit"
+                        style={{ gridTemplateColumns: `repeat(${cols}, minmax(min-content, 1fr))` }}
+                    >
+                        {[...Array(rows)].map((_, row) =>
+                            [...Array(cols)].map((_, col) => (
+                                <div
+                                    key={`${row}-${col}`}
+                                    className={cn(
+                                        "bg-wood-200 -mt-[50px] h-[100px] w-[86.6px] scale-[0.89] hover:opacity-80",
+                                        row === 0 && "mt-0",
+                                        (row + col) % 2 === 0
+                                            ? "triangle-left origin-[66.5%_50%]"
+                                            : "triangle-right origin-[33.4%_50%]",
 
-                                    (row + col) % 2 === 0 && row === 0 && "triangle-left-first",
-                                    (row + col) % 2 === 0 && row === rows - 1 && "triangle-left-last",
+                                        (row + col) % 2 === 0 && row === 0 && "triangle-left-first",
+                                        (row + col) % 2 === 0 && row === rows - 1 && "triangle-left-last",
 
-                                    (row + col) % 2 !== 0 && row === 0 && "triangle-right-first",
-                                    (row + col) % 2 !== 0 && row === rows - 1 && "triangle-right-last",
-                                )}
-                            />
-                        )),
-                    )}
-                </div>
-            </TransformComponent>
-        </TransformWrapper>
+                                        (row + col) % 2 !== 0 && row === 0 && "triangle-right-first",
+                                        (row + col) % 2 !== 0 && row === rows - 1 && "triangle-right-last",
+                                    )}
+                                />
+                            )),
+                        )}
+                    </div>
+                </TransformComponent>
+            </TransformWrapper>
+        </div>
     );
 }
-
-// 100 160px
-// 3 5.12px 2.56px
